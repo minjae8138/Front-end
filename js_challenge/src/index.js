6690e28360b3fe7d0cbb9b1f6ee34442870e911e
minjae <alswo8138@gmail.com>
2021-01-14 22:28:13 +0900

    change

--- js_challenge/src/index.js
+++ js_challenge/src/index.js
@@ -1,40 +1,24 @@
-// <⚠️ DONT DELETE THIS ⚠️>
-// import "./styles.css";
-const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
-// <⚠️ /DONT DELETE THIS ⚠️>
+const body = document.querySelector("body");
+const colors = ["#3498db", "#9b59b6", "#f39c12"];
 
-/*
-✅ The text of the title should change when the mouse is on top of it.
-✅ The text of the title should change when the mouse is leaves it.
-✅ When the window is resized the title should change.
-✅ On right click the title should also change.
-✅ The colors of the title should come from a color from the colors array.
-✅ DO NOT CHANGE .css, or .html files.
-✅ ALL function handlers should be INSIDE of "superEventHandler"
-*/
-const superEventHandler = {
-  handleMouseOver: function () {
-    h2.innerHTML = "The mouse is here!";
-    h2.style.color = colors[0];
-  },
-  handleMouseLeave: function () {
-    h2.innerHTML = "The mouse is gone";
-    h2.style.color = colors[1];
-  },
-  handleResize: function () {
-    h2.innerHTML = "You just resized!";
-    h2.style.color = colors[2];
-  },
-  handleRightClick: function () {
-    h2.innerHTML = "That was a right click!";
-    h2.style.color = colors[4];
+function handleColor(){
+  const windowSize = window.innerWidth;
+  if (windowSize  <700){
+    body.style.backgroundColor = colors[0] ;
+    console.log(windowSize)
+  }
+  else if(windowSize < 1000  ){
+    body.style.backgroundColor = colors[1] ;
+    console.log(windowSize)
+  }
+  else{
+    body.style.backgroundColor = colors[2];
+    console.log(windowSize)
   }
 };
 
+function init(){
+  window.addEventListener("resize",handleColor)
+};
 
-const h2 = document.querySelector("h2");
-
-h2.addEventListener("mouseover", superEventHandler.handleMouseOver);
-h2.addEventListener("mouseout", superEventHandler.handleMouseLeave);
-window.addEventListener("resize", superEventHandler.handleResize);
-window.addEventListener("contextmenu", superEventHandler.handleRightClick);
+init();
